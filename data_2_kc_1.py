f6a7c26147cda2bd315b681a9a0ad928051d07f7
Richard Oden <richard.thomas.oden@gmail.com>
2022-05-23 18:29:25 -0400

    In data_2_kc_1, added code to get number of spells by level

--- data_2_kc_1.py
+++ data_2_kc_1.py
@@ -5,3 +5,9 @@ schools = ['conjuration', 'necromancy', 'evocation', 'abjuration', 'transmutatio
 for school in schools:
     json_response = requests.get(f'https://www.dnd5eapi.co/api/spells?school={school}').json()
     print(f'Number of {school} spells: {len(json_response["results"])}')
+
+level = 1
+while level < 10:
+    json_response = requests.get(f'https://www.dnd5eapi.co/api/spells?level={level}').json()
+    print(f'Number of level {level} spells: {len(json_response["results"])}')
+    level += 1
