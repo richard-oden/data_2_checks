ea29455dd62eff2d297dc27a96c1e6dcf70cc50a
Richard Oden <richard.thomas.oden@gmail.com>
2022-05-23 18:52:40 -0400

    In data_2_kc_1, added code to retrieve evocation cantrips, and logic for parsing average damage

--- data_2_kc_1.py
+++ data_2_kc_1.py
@@ -1,13 +1,31 @@
-import requests
 import pandas as pd
+import requests
+import re
 
+# 1. Find and print TWO descriptive statistics about your data.
+
+# Number of spells in each school of magic in D&D 5e.
 schools = ['conjuration', 'necromancy', 'evocation', 'abjuration', 'transmutation', 'divination', 'enchantment', 'illusion']
 for school in schools:
-    json_response = requests.get(f'https://www.dnd5eapi.co/api/spells?school={school}').json()
-    print(f'Number of {school} spells: {len(json_response["results"])}')
+    spells_json = requests.get(f'https://www.dnd5eapi.co/api/spells?school={school}').json()
+    print(f'Number of {school} spells: {len(spells_json["results"])}')
 
-level = 1
+# Number of spells by spell level in D&D 5e.
+level = 0
 while level < 10:
-    json_response = requests.get(f'https://www.dnd5eapi.co/api/spells?level={level}').json()
-    print(f'Number of level {level} spells: {len(json_response["results"])}')
+    spells_json = requests.get(f'https://www.dnd5eapi.co/api/spells?level={level}').json()
+    print(f'Number of level {level} spells: {len(spells_json["results"])}')
     level += 1
+
+# 2. Write a query in Pandas to select a particular set of your data.
+
+# Get details for all evocation cantrips:
+evocation_cantrips_json = requests.get('https://www.dnd5eapi.co/api/spells?school=evocation&level=0').json()
+cantrip_details = []
+for cantrip in evocation_cantrips_json['results']:
+    cantrip_details.append(requests.get(f'https://www.dnd5eapi.co/api/spells/{cantrip["index"]}').json())
+
+dice_regex = r'(\d+)?d(\d+)([\+\-]\d+)?'
+
+def get_average_damage(amount, num_sides, modifier):
+    return (((num_sides + 1) / 2) * amount) + (modifier or 0)
