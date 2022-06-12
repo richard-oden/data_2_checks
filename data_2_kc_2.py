913dca8e800a9e088f1c9d48ab940c7d1a1b902c
Richard Oden <richard.thomas.oden@gmail.com>
2022-06-12 14:15:54 -0400

    Added code to retrieve artist information for Knower

--- data_2_kc_2.py
+++ data_2_kc_2.py
@@ -8,6 +8,7 @@ import requests
 class BearerAuth(requests.auth.AuthBase):
     def __init__(self, token):
         self.token = token
+
     def __call__(self, r):
         r.headers["authorization"] = "Bearer " + self.token
         return r
@@ -18,6 +19,7 @@ client_id = os.environ.get('SPOTIFY_CLIENT_ID')
 client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET')
 username = 'rqfju899d4ie81memjx0fgy70'
 
+# Authenticate application and get access token:
 client_auth_response = requests.post(
     url=f'https://accounts.spotify.com/api/token', 
     auth=HTTPBasicAuth(client_id, client_secret), 
@@ -27,7 +29,10 @@ if not client_auth_response.ok:
     raise RuntimeError(f'There was a problem retrieving application credentials from spotify: \n\t{client_auth_response.content}')
 
 access_token = client_auth_response.json()['access_token']
-user_response = requests.get(f'https://api.spotify.com/v1/users/{username}', auth=BearerAuth(access_token))
 
-user_data = user_response.json()
-print(user_data)
+# Get artist information for the band Knower:
+artist_info = requests.get(
+    url='https://api.spotify.com/v1/artists/7fVp0A6oCMfiQJihMnY0SZ', 
+    auth=BearerAuth(access_token)).json()
+
+print(artist_info)
