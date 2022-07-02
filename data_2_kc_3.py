f0b267dec9011c49e388b6c11f0f20386aafe5ef
Richard Oden <richard.thomas.oden@gmail.com>
2022-07-02 18:41:19 -0400

    Changed sample year for knowledge check 3 to 2020 due to lack of data from World Bank in 2021.

--- data_2_kc_3.py
+++ data_2_kc_3.py
@@ -10,35 +10,33 @@ class ColumnName(enum.Enum):
 
 # Import data from income per capita report from World Bank:
 # https://data.worldbank.org/indicator/NY.ADJ.NNTY.PC.CD
-wb_df = pd.read_csv(os.path.join('data', 'API_NY.ADJ.NNTY.PC.CD_DS2_en_csv_v2_4261468.csv'), usecols=('Country Name', '2021'))
+wb_df = pd.read_csv(os.path.join('data', 'API_NY.ADJ.NNTY.PC.CD_DS2_en_csv_v2_4261468.csv'), usecols=('Country Name', '2020'))
 
 # Rename columns:
 wb_df.rename(columns={
     'Country Name': ColumnName.COUNTRY.value,
-    '2021': ColumnName.OVERALL_INCOME.value
+    '2020': ColumnName.OVERALL_INCOME.value
 }, inplace=True)
-
-print(wb_df.sample(30))
 
 # Drop rows with null values:
 wb_df.dropna(inplace=True)
 
 # Import data from 2021 Stack Overflow developer survey:
 # https://insights.stackoverflow.com/survey
-so_df = pd.read_csv(os.path.join('data', 'survey_results_public.csv'), usecols=('Country', 'ConvertedCompYearly'))
+so_df = pd.read_csv(os.path.join('data', 'survey_results_public.csv'), usecols=('Country', 'ConvertedComp'))
 
 # Rename columns:
 so_df.rename(columns={
-    'ConvertedCompYearly': ColumnName.DEVELOPER_INCOME.value
+    'ConvertedComp': ColumnName.DEVELOPER_INCOME.value
 }, inplace=True)
 
 # Drop rows with null values:
 so_df.dropna(inplace=True)
 
 # Group by country and get average income:
-so_df.groupby(ColumnName.COUNTRY.value)[ColumnName.DEVELOPER_INCOME.value].mean()
+so_df = so_df.groupby(ColumnName.COUNTRY.value, as_index=False)[ColumnName.DEVELOPER_INCOME.value].mean()
 
 # Merge dataframes
 merged_df = wb_df.merge(so_df, on=ColumnName.COUNTRY.value)
 
-print(merged_df.sample(30))
+print(merged_df)
