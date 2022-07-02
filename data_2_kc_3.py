3b0abe7cdec12a5f96192409462d201654d3ebb4
Richard Oden <richard.thomas.oden@gmail.com>
2022-07-02 18:15:52 -0400

    Fixed various bugs. Grouped Stack Overflow survey responses by countries and averaged income. Merged dataframes.

--- data_2_kc_3.py
+++ data_2_kc_3.py
@@ -2,28 +2,43 @@ import enum
 import os
 import pandas as pd
 
+# I'm using an enum to store column names.
 class ColumnName(enum.Enum):
-    COUNTRY = 'Country',
-    DEVELOPER_INCOME = 'Average Developer Income in USD ($)',
+    COUNTRY = 'Country'
+    DEVELOPER_INCOME = 'Average Developer Income in USD ($)'
     OVERALL_INCOME = 'Income per Capita in USD ($)'
 
 # Import data from income per capita report from World Bank:
 # https://data.worldbank.org/indicator/NY.ADJ.NNTY.PC.CD
 wb_df = pd.read_csv(os.path.join('data', 'API_NY.ADJ.NNTY.PC.CD_DS2_en_csv_v2_4261468.csv'), usecols=('Country Name', '2021'))
 
-wb_df.rename({
+# Rename columns:
+wb_df.rename(columns={
     'Country Name': ColumnName.COUNTRY.value,
     '2021': ColumnName.OVERALL_INCOME.value
-})
+}, inplace=True)
 
+print(wb_df.sample(30))
+
+# Drop rows with null values:
 wb_df.dropna(inplace=True)
 
 # Import data from 2021 Stack Overflow developer survey:
 # https://insights.stackoverflow.com/survey
 so_df = pd.read_csv(os.path.join('data', 'survey_results_public.csv'), usecols=('Country', 'ConvertedCompYearly'))
 
-wb_df.rename({
+# Rename columns:
+so_df.rename(columns={
     'ConvertedCompYearly': ColumnName.DEVELOPER_INCOME.value
-})
+}, inplace=True)
 
+# Drop rows with null values:
 so_df.dropna(inplace=True)
+
+# Group by country and get average income:
+so_df.groupby(ColumnName.COUNTRY.value)[ColumnName.DEVELOPER_INCOME.value].mean()
+
+# Merge dataframes
+merged_df = wb_df.merge(so_df, on=ColumnName.COUNTRY.value)
+
+print(merged_df.sample(30))
